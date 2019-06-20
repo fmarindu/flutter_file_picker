2b453e7d98f01b1affc82dba1fb6ac5c1d73f7c6
Fredy Marin <fredymarinduarte@gmail.com>
2019-06-20 10:58:00 -0500

    It is add file-picker

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our 
 [online documentation](https://flutter.dev/docs), which offers tutorials, 
 samples, guidance on mobile development, and a full API reference.
+
+file_picker
+    Version file_picker:
+        1.3.6
+    URL:
+        https://pub.dev/packages/file_picker/versions/1.3.6
--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,144 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'package:flutter/services.dart';
+import 'package:file_picker/file_picker.dart';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _fileName = '...';
+  String _path = '...';
+  String _extension;
+  bool _hasValidMime = false;
+  FileType _pickingType;
+  TextEditingController _controller = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() => _extension = _controller.text);
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  void _openFileExplorer() async {
+    if (_pickingType != FileType.CUSTOM || _hasValidMime) {
+      try {
+        _path = await FilePicker.getFilePath(type: _pickingType, fileExtension: _extension);
+      } on PlatformException catch (e) {
+        print("Unsupported operation" + e.toString());
+      }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+      if (!mounted) return;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+      setState(() {
+        _fileName = _path != null ? _path.split('/').last : '...';
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: SingleChildScrollView(
+          child: new Center(
+              child: new Padding(
+                padding: const EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: new DropdownButton(
+                          hint: new Text('LOAD PATH FROM'),
+                          value: _pickingType,
+                          items: <DropdownMenuItem>[
+                            new DropdownMenuItem(
+                              child: new Text('FROM AUDIO'),
+                              value: FileType.AUDIO,
+                            ),
+                            new DropdownMenuItem(
+                              child: new Text('FROM GALLERY'),
+                              value: FileType.IMAGE,
+                            ),
+                            new DropdownMenuItem(
+                              child: new Text('FROM VIDEO'),
+                              value: FileType.VIDEO,
+                            ),
+                            new DropdownMenuItem(
+                              child: new Text('FROM ANY'),
+                              value: FileType.ANY,
+                            ),
+                            new DropdownMenuItem(
+                              child: new Text('CUSTOM FORMAT'),
+                              value: FileType.CUSTOM,
+                            ),
+                          ],
+                          onChanged: (value) => setState(() => _pickingType = value)),
+                    ),
+                    new ConstrainedBox(
+                      constraints: new BoxConstraints(maxWidth: 150.0),
+                      child: _pickingType == FileType.CUSTOM
+                          ? new TextFormField(
+                        maxLength: 20,
+                        autovalidate: true,
+                        controller: _controller,
+                        decoration: InputDecoration(labelText: 'File type'),
+                        keyboardType: TextInputType.text,
+                        textCapitalization: TextCapitalization.none,
+                        validator: (value) {
+                          RegExp reg = new RegExp(r'[^a-zA-Z0-9]');
+                          if (reg.hasMatch(value)) {
+                            _hasValidMime = false;
+                            return 'Invalid format';
+                          }
+                          _hasValidMime = true;
+                        },
+                      )
+                          : new Container(),
+                    ),
+                    new Padding(
+                      padding: const EdgeInsets.only(top: 50.0, bottom: 20.0),
+                      child: new RaisedButton(
+                        onPressed: () => _openFileExplorer(),
+                        child: new Text("Open file picker"),
+                      ),
+                    ),
+                    new Text(
+                      'URI PATH ',
+                      textAlign: TextAlign.center,
+                      style: new TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    new Text(
+                      _path ?? '...',
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      textScaleFactor: 0.85,
+                    ),
+                    new Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: new Text(
+                        'FILE NAME ',
+                        textAlign: TextAlign.center,
+                        style: new TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    new Text(
+                      _fileName,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              )),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
